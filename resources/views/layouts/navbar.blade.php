218f43f30fab96531cbb07d1501dfc61b2adc7c5
jarasa03 <javierarruabarrena03@gmail.com>
2025-03-14 22:11:51 +0100

    style: :lipstick: Mejora de estilos en la barra de navegación

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,18 +1,21 @@
 <nav class="navbar">
+    <!-- Sección Izquierda: Logo -->
     <div class="navbar__logo">
         <a href="{{ route('dashboard') }}">🏐 VolleyQuiz</a>
     </div>
+
+    <!-- Sección Central: Links -->
     <div class="navbar__links">
         <a href="#" class="navbar__link">Documentación</a>
         <a href="#" class="navbar__link">Arcade</a>
         <a href="#" class="navbar__link">Zen</a>
         <a href="#" class="navbar__link">Niveles</a>
         <a href="#" class="navbar__link">Ranking</a>
+    </div>
 
-        <!-- Perfil y Cerrar Sesión -->
-        <div class="navbar__profile">
-            <a href="{{ route('users.perfil') }}" class="navbar__profile-btn">{{ auth()->user()->name }}</a>
-        </div>
+    <!-- Sección Derecha: Perfil y Cerrar Sesión -->
+    <div class="navbar__profile-actions">
+        <a href="{{ route('users.perfil') }}" class="navbar__profile-btn">{{ auth()->user()->name }}</a>
 
         <form action="{{ route('auth.logout') }}" method="POST" class="navbar__logout-form">
             @csrf
