7802c8171ff91ef7b7a59d92bc531f67f764baff
jarasa03 <javierarruabarrena03@gmail.com>
2025-05-27 23:35:20 +0200

    style: :lipstick: Efecto de scroll para la navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -65,3 +65,14 @@ document.querySelectorAll('.faq-question').forEach(button => {
         }
     });
 });
+
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+    if (!navbar) return;
+
+    if (window.scrollY > 10) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
